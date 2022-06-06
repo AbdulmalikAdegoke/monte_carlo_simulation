0fcad6749b3385f079042551b47e29453cae19a8
Abdulmalik <abdulmalikadegoke@gmail.com>
2022-06-06 19:06:24 +0100

    imported some relevant libraries and made some corrections

---
+++ monte_carlo_app/src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
