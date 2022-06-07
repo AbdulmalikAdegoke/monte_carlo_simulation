2192037660ad0da75cda2e7e4482c147b6ae628a
Abdulmalik <abdulmalikadegoke@gmail.com>
2022-06-07 03:18:23 +0100

    added the page contents for the login and registration pages

--- monte_carlo_app/src/LogIn.js
+++ monte_carlo_app/src/LogIn.js
@@ -2,6 +2,37 @@ import React from 'react'
 
 export default function LogInPage() {
   return (
-    <div>LogInPage</div>
+    <div>
+      <div>
+        <div>
+          <h1>LogIn</h1>
+          <p>Enter your login details here</p>
+          {/* login form */}
+          <form>
+            <label for="username">User name:</label><br/>
+            <input type="text" id='username' name="username" placeholder="Enter user name here" /><br/>
+            <label for="password">Password:</label><br/>
+            <input type="text" id='password' name="password" /><br/>
+
+            <div>
+              <div>
+                <input type="checkbox" id='rememberme' name='rememberme'/>
+                <label for="rememberme">Remember Me</label>
+              </div>
+              <div>
+                <a href='#'>Forgot Password?</a>
+              </div>
+            </div>
+
+            <input type="submit" value="Submit" />
+          </form>
+          
+          <p>Don't have an account?<a href='#'>Register</a></p>
+        </div>
+        
+      </div>
+      
+
+    </div>
   )
 }
--- monte_carlo_app/src/Registration.js
+++ monte_carlo_app/src/Registration.js
@@ -2,6 +2,36 @@ import React from 'react'
 
 export default function RegistrationPage() {
   return (
-    <div>RegistrationPage</div>
+    <div>
+      <div>
+        <div>
+          <h1>Register</h1>
+          <p>Register your login details here</p>
+          {/* registration form */}
+          <form>
+            <label for="username">User name:</label><br/>
+            <input type="text" id='username' name="username" placeholder="Enter user name here" /><br/>
+            <label for="email">Email Address:</label><br/>
+            <input type="text" id='email' name="email" /><br/>
+            <label for="password">Password:</label><br/>
+            <input type="text" id='password' name="password" /><br/>
+
+            <div>
+              <div>
+                <input type="checkbox" id='rememberme' name='rememberme'/>
+                <label for="rememberme">Remember Me</label>
+              </div>
+              <div>
+                <a href='#'>Forgot Password?</a>
+              </div>
+            </div>
+
+            <input type="register" value="Register" />
+          </form>
+          <p>Already have an account?<a href='#'>Login</a></p>
+        </div>
+        
+      </div>
+    </div>
   )
 }
